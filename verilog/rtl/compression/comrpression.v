fe0f59836cd2155973444275cedc507ede65d4e2
jetsama <jonathan.e.hess@gmail.com>
2023-10-25 21:53:21 -0500

    Added compression and updated memory controller

---
+++ verilog/rtl/compression/comrpression.v
@@ -0,0 +1,15 @@
+/*
+compression module
+
+*/
+module compression (
+    input clk,
+    input adc_clock,
+    input [15:0] thres,
+    input [15:0] slope,
+    input [15:0] data_in
+    output [15:0] data_out
+    );
+
+
+endmodule
--- verilog/rtl/memory/memorycontroller.v
+++ verilog/rtl/memory/memorycontroller.v
@@ -6,11 +6,13 @@ module memorycontroller (
     input clk,
     input adc_clock,
     input record,
-    input loop,off_chip_mem,
+    input loop,
+    input off_chip_mem,
     input [15:0] delay_reverve,
     input [15:0] gain,
-    input [15:0] impulses
-    output [15:0] impulse_output
+    input [15:0] impulses,
+    input [15:0] data_in
+    output [15:0] data_out
     );
 
 
