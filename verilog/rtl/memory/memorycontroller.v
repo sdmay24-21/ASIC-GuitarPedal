1568314935bf1141ee094e40b462ea852ce44841
jetsama <jonathan.e.hess@gmail.com>
2023-10-25 21:14:21 -0500

    added memory controller stub and updated diagram

---
+++ verilog/rtl/memory/memorycontroller.v
@@ -0,0 +1,17 @@
+/*
+memory control module
+
+*/
+module memorycontroller (
+    input clk,
+    input adc_clock,
+    input record,
+    input loop,off_chip_mem,
+    input [15:0] delay_reverve,
+    input [15:0] gain,
+    input [15:0] impulses
+    output [15:0] impulse_output
+    );
+
+
+endmodule
