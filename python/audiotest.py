57c8a474c5b4381b2cb334fa2c6240a2e1ef1b6f
Jonathan Hess <84877315+Jetsama@users.noreply.github.com>
2023-11-14 19:03:53 -0600

    Impulse Audio Testing

--- python/audiotest.py
+++ python/audiotest.py
@@ -5,14 +5,43 @@ import pandas as pd
 
 #from audio import wavToCSV
 from pedal import pedal
+
+
 input_filename = "python/audio/test1.wav"
-output_filename = "python/audiotests/test1_resampled.wav"
+
 output_filename_compress = "python/audiotests/test1_compress.wav"
 
+output_filename = "python/audiotests/test0_4bit.wav"
+test0 = pedal(input_filename,output_filename,data_bits= 4) # change
+test0.compression(1,1) 
+test0.write()
 
-test1 = pedal(input_filename,output_filename_compress)
-test1.compression(.5,0)
+output_filename = "python/audiotests/test1_8bit.wav"
+test1 = pedal(input_filename,output_filename,data_bits= 8) # change
+test1.compression(1,1) 
 test1.write()
+
+output_filename = "python/audiotests/test2_16bit.wav"
+test2 = pedal(input_filename,output_filename,data_bits= 16) #no change
+test2.compression(1,1) 
+test2.write()
+
+print("start impulse")
+output_filename = "python/audiotests/test3_impulse.wav"
+impulse_filename = "python/impulses/DAMPEDCAVE.wav"
+test3 = pedal(input_filename,output_filename,data_bits= 16) #no change
+test3.setimpulse(impulse_filename)
+test3.FIR(1000,1,1) 
+test3.write()
+
+print("start impulse2")
+output_filename = "python/audiotests/test4_impulse.wav"
+impulse_filename = "python/impulses/WATER.wav"
+test4 = pedal(input_filename,output_filename,data_bits= 16) #no change
+test4.setimpulse(impulse_filename)
+test4.FIR(10000,1,1) 
+test4.write()
+
 
 #output_sample_rate = 10000 #10k
 #samrate, data = wavfile.read(input_filename)
--- python/pedal.py
+++ python/pedal.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 
 class pedal:
-    def __init__(self,input_file,output_file,samplerate = 10000) -> None:
+    def __init__(self,input_file,output_file,samplerate = 10000,data_bits= 16,control_bits = 8) -> None:
         
         self.input_file = input_file
         self.output_file = output_file
         self.samplerate = samplerate
+        self.bits = data_bits
+        self.control_bits = control_bits
         #resample data
         samrate, data = wavfile.read(input_file)
         resample_ratio = float(samplerate) / samrate
@@ -25,20 +27,21 @@ class pedal:
         #normalize
         norm = (mono - self.min_data) / (self.max_data - self.min_data)
         #convert to 16 bit
-        self.data = (norm * 32767).astype(np.int16)
+        self.maxvalue = 2**(self.bits-1) #2 to the bits power divided by 2 for signed/unsigned
+        self.data = (norm * self.maxvalue).astype(np.int16)
         
         
     def write(self):
         #convert back to float and
         #un normalize
-        norm = (self.data.astype(np.float32) / 32767) * (self.max_data - self.min_data) + self.min_data
+        norm = (self.data.astype(np.float32) / self.maxvalue) * (self.max_data - self.min_data) + self.min_data
         
         wavfile.write(self.output_file, self.samplerate, norm.astype('int16'))
-    def compress(elem, thres, slope2):
+    def compress(elem, thres, slope2,maxvalue):
         #compress code
         slope1 = (1 - (slope2*(1-thres)))/thres
-        if(abs(elem) > thres*32767):
-            result = elem*slope2 + thres*32767
+        if(abs(elem) > thres*maxvalue):
+            result = elem*slope2 + thres*maxvalue
         else:
             result = elem* slope1
         return result
@@ -47,7 +50,44 @@ class pedal:
         #self.thres = thres
         #self.slope = slope
         vcompress = np.vectorize(pedal.compress)
-        compressedData = vcompress(self.data,thres,slope)
+        compressedData = vcompress(self.data,thres,slope,self.maxvalue)
         self.data = compressedData
-
         
+    def setimpulse(self,input_file):
+        #resample data
+        samrate, data = wavfile.read(input_file)
+        resample_ratio = float(self.samplerate) / samrate
+        resampled_data = resample(data, int(len(data) * resample_ratio))
+        mono = resampled_data.sum(axis=1) / 2
+        self.impulses = mono #TODO bits?
+        
+    def impulse(self,data,num_impulses, gain,delay_reverb):
+        result = 0
+        count = 0
+        
+        length = min(len(data),num_impulses)
+        
+        dataRes = np.array(data[:length])
+        impulseRes = np.array(self.impulses[:length])
+        result = np.sum(np.multiply(dataRes,impulseRes*self.maxvalue))
+        return result
+    
+        for (impulse,datum) in zip(self.impulses,data):
+            count+=1
+            if(count>num_impulses):
+                return result
+            result+= impulse*self.maxvalue*datum
+        return result
+    
+    def FIR(self,impulses,gain,delay_reverb):
+        
+        data = list(self.data)
+        result = list()
+        datum = data.pop(0)
+        count = 0
+        for c in range(len(data)):
+            count+=1
+            result.append(self.impulse(data, impulses,gain,delay_reverb))
+                    
+            datum = data.pop(0)
+        self.data =  np.array(result)
