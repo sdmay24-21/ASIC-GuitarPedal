a35a3490bd32d92602541104619e1cda62ea7693
Samuel Heikens <sheikens@iastate.edu>
2024-04-02 17:16:01 -0500

    Hardening

--- verilog/rtl/memory/multiplier.v
+++ verilog/rtl/memory/multiplier.v
@@ -30,9 +30,18 @@ module memorycontroller /*(
   + (|data_in) ? (data_in<<6): 0
   + (|data_in) ? (data_in<<7): 0 
 
-+ (data_in & (8'h01)) ? (data_in << 8) : 8'h00
 
- // + (data_in&(0x01<<0)) ? (data_in<<8): 0 
+  + (data_in & (8'h01)) ? (data_in << 8) : 8'h00
+  + (data_in & (8'h02)) ? (data_in << 9) : 8'h00
+  + (data_in & (8'h04)) ? (data_in << 10) : 8'h00
+  + (data_in & (8'h08)) ? (data_in << 11) : 8'h00
+  + (data_in & (8'h10)) ? (data_in << 12) : 8'h00
+  + (data_in & (8'h20)) ? (data_in << 13) : 8'h00
+  + (data_in & (8'h40)) ? (data_in << 14) : 8'h00
+  + (data_in & (8'h80)) ? (data_in << 15) : 8'h00
+
+/*
+  + (data_in&(0x01<<0)) ? (data_in<<8): 0 
   + (data_in&(0x01<<1)) ? (data_in<<9): 0
   + (data_in&(0x01<<2)) ? (data_in<<10): 0
   + (data_in&(0x01<<3)) ? (data_in<<11): 0//p
@@ -40,6 +49,7 @@ module memorycontroller /*(
   + (data_in&(0x01<<5)) ? (data_in<<13): 0
   + (data_in&(0x01<<6)) ? (data_in<<14): 0
   + (data_in&(0x01<<7)) ? (data_in<<15): 0
+  */
   );
 
 
