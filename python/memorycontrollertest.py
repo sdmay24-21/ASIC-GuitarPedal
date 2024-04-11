ab52ada6ce0b89c20fa8bde56354c4f814acc4c8
jetsama <jehess@iastate.edu>
2024-04-11 13:23:01 -0500

    Memory Controller Python

--- python/memorycontrollertest.py
+++ python/memorycontrollertest.py
@@ -2,6 +2,6 @@ from memorycontroller import MemoryController
 
 input_filename = "python/audio/test1.wav"
 impulse_filename = "python/impulses/DAMPEDCAVE.wav"
-outputfile = "python/data/memorycontroller.csv"
+outputfile = "python/data/memorycontroller"
 memCtrlr = MemoryController();
 memCtrlr.createDataFromAudio(outputfile,input_filename,impulse_filename,);
